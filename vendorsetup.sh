4d2530fe47806ebf9d8fd730732a16fe214aab1b
khusnan21 <khusnan21@gmail.com>
2025-08-24 16:25:21 +0700

    Update vendorsetup.sh
    Signed-off-by: khusnan21 <khusnan21@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 export LC_ALL="C"
+
+# Apply recovery patches
 cd bootable/recovery
-
 for p in ../../device/itel/P1102GT/patches/*; do
-if ! git am -3 <$p; then
-    # Force use fuzzy patch
-    patch -p1 <$p
-    git add .
-    git am --continue || {
-        echo "Failed to apply $p"
-        exit 1
-    }
-fi
+    if ! git am -3 <"$p"; then
+        echo "Fuzzy applying $p"
+        patch -p1 <"$p"
+        git add .
+        git am --continue || {
+            echo "Failed to apply $p"
+            exit 1
+        }
+    fi
 done
-
 cd ../../
+
+# Register lunch combo
+add_lunch_combo twrp_P1102GT-eng
